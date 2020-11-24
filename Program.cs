1d3a5c22abfbf257437eb588d8479a48cee79235
Regis Leandro da Silva Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-24 13:07:48 -0300

    Este programa calcula a tabuada usando o conceito de funções

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Auladia20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void tabuada(int numero) {
+               for (int i=1;i<=10;i++){
+                   Console.WriteLine(numero+ "x" +i+ "=" +(numero*i));
+               }
+
+            }
+
+            tabuada(4);
+        }
+
+    }
+}
